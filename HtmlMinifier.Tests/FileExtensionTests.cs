69608e97120ac7b21baba098cf5b9be382cc7e40
HelderSepu <HelderSepu@gmail.com>
2016-04-18 12:12:50 -0400

    Add test case for bug #25

---
+++ HtmlMinifier.Tests/FileExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HtmlMinifier.Tests
+{
+    [TestFixture]
+    public class FileExtensionTests
+    {
+        [Test]
+        public void GithubIssue25__ShouldReturnCorrectly()
+        {                        
+            Assert.That("test.html".IsHtmlFile(), Is.True);
+            Assert.That("codes.js.aspx".IsHtmlFile(), Is.True);
+
+            Assert.That("codes.aspx.js".IsHtmlFile(), Is.False);
+            Assert.That("aspx.codes.js".IsHtmlFile(), Is.False);
+        }
+    }
+}
